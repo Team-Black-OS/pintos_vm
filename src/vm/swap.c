0933e54f42e7e5627cb499c16a348269bb76d91e
Andrew0Hill <andrew.2.hill@ucdenver.edu>
2017-04-25 13:47:00 -0600

    Added swap.c and swap.h

---
+++ src/vm/swap.c
@@ -0,0 +1,11 @@
+#include "vm/swap.h"
+#include "devices/block.h"
+#include <bitmap.h>
+#include "threads/synch.h"
+#include "threads/vaddr.h"
+
+#define PAGE_SECTORS PGSIZE/BLOCK_SECTOR_SIZE
+static struct block* global_swap;
+static struct bitmap global_bitmap;
+static struct lock swap_lock;
+
---
+++ src/vm/swap.h
@@ -0,0 +1,6 @@
+#ifndef _SWAP_H
+#define _SWAP_H
+
+struct page; 
+
+#endif
